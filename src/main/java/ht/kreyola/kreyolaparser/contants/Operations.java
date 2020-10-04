276967d58589c09bb75413c23961fe8ebc6791b3
Wislet Michel <wisletmichel@gmail.com>
2020-10-04 09:40:13 -0500

    try to detect operations type (read/write, input/output)

---
+++ src/main/java/ht/kreyola/kreyolaparser/contants/Operations.java
@@ -0,0 +1,92 @@
+package ht.kreyola.kreyolaparser.contants;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Operations {
+    public static final List<String> inputOperations = Arrays.asList(
+            "saisir",
+            "saisit",
+            "entrer",
+            "entre",
+            "sélectionner",
+            "sélectionne",
+            "choisir",
+            "choisit",
+            "terminer",
+            "termine"
+    );
+    public static final List<String> outputOperations = Arrays.asList(
+            "imprimer",
+            "imprime",
+            "afficher",
+            "affiche",
+            "répertorier",
+            "répertorie",
+            "retourner",
+            "retourne",
+            "lister",
+            "liste",
+            "confirmer",
+            "confirme"
+    );
+
+    public static final List<String> readOperations = Arrays.asList(
+            "vérifier",
+            "vérifie",
+            "rechercher",
+            "recherche",
+            "lire",
+            "lit",
+            "visualiser",
+            "visualise",
+            "diffuser",
+            "diffuse",
+            "examiner",
+            "examine",
+            "explorer",
+            "explore",
+            "analyser",
+            "analyse"
+
+    );
+    public static final List<String> writeOperations = Arrays.asList(
+            "enregistrer",
+            "enregistre",
+            "sauvegarder",
+            "sauvegarde",
+            "insérer",
+            "insére",
+            "créer",
+            "crée",
+            "copier",
+            "copie",
+            "coller",
+            "colle",
+            "ajouter",
+            "ajoute",
+            "écrire",
+            "écrit",
+            "authentifier",
+            "authentifie",
+            "connecter",
+            "connecte",
+            "archiver",
+            "archive",
+            "consigner",
+            "consigne",
+            "modifier",
+            "modifie",
+            "mettre à jour",
+            "met à jour",
+            "supprimer",
+            "supprime",
+            "annuler",
+            "annule",
+            "effacer",
+            "efface",
+            "activer",
+            "active"
+    );
+
+}
